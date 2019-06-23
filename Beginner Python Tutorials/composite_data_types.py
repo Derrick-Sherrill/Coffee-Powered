018acc7496df99ca8418b5a9e279c900b8a168f0
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-06-23 13:33:42 +0100

    update for composite data types

---
+++ Beginner Python Tutorials/composite_data_types.py
@@ -0,0 +1,16 @@
+# list, tuple, dictionary, frozenset, set
+
+our_list = ["a","b","c", 1.5] # Creating a list
+print(our_list) # printing our list to the terminal
+
+our_tuple = ("a","b","c") #Creating a tuple
+print(our_tuple) #printing our tuple to the terminal
+
+our_dictionary = {"name":"Derrick", "height":"6'0"} #Creating a dictionary 
+print(our_dictionary) #printing dictionary to the terminal
+
+our_set = {"one","two","three","two"} #Creating a set
+print(our_set) #printing our set to the terminal
+
+our_frozenset = frozenset(our_set) #Taking our set and making a frozen set with it
+print(our_frozenset) # printing our frozen set to the terminal
